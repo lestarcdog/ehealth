dedd0fe1981ee2713174b5ac632a3c6873d9a790
cdog <cdog@cdogmainframe>
2015-04-05 00:21:39 +0200

    web app

---
+++ ehealth/ehealth-web/src/main/webapp/js/apps/ehealthApp.js
@@ -0,0 +1,10 @@
+var app = angular.module('ehealthApp', ["ngRoute"]);
+app.controller("MainController", [function() {
+	this.notes = [
+	              {id : 1 , msg :"aaaaa"},
+	              {id : 2 , msg :"bbbb"},
+	              {id : 3 , msg :"cccc"},
+	              {id : 4 , msg :"ddddd"},
+	              ];
+	console.log("valami");
+}]);
---
+++ ehealth/ehealth-web/src/test/java/atmosphere/AtmosphereTest.java
@@ -0,0 +1,13 @@
+package atmosphere;
+
+import org.atmosphere.cpr.AtmosphereFramework;
+import org.junit.Test;
+
+public class AtmosphereTest {
+
+  @Test
+  public void test1() {
+    AtmosphereFramework f = new AtmosphereFramework();
+  }
+
+}
